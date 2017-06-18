783da8bdd1356f2058a4301fc9d12ca8e0a19b43
Lybron Sobers <lybron.developer@gmail.com>
2017-06-17 22:40:06 -0400

    Feat: Added Store View Controllers

--- showing views/StoreViewController.swift
+++ showing views/StoreViewController.swift
@@ -8,28 +8,26 @@
 
 import UIKit
 
-class StoreViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class StoreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+  
+  // MARK: Properties
+  let cellIdentifier = "Cell"
+  
+  // MARK: Datasource
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    return UICollectionViewCell()
+  }
+  
+  // MARK: Delegate
+  
 }
