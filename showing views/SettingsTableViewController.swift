4f08040f688135dfc66139d6e7a7f7665dfed387
SNup  dUG <nicholas.ramcharitar@icloud.com>
2017-11-26 11:29:29 -0400

    Started settings controller

---
+++ showing views/SettingsTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsTableViewController.swift
+//  STAR WARS CARDS
+//
+//  Created by Nicholas Ramcharitar on 26/11/2017.
+//  Copyright © 2017 Nicholas. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
